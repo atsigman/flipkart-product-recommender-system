c3cb0241711674c4ad9ec4d6717d13ca8a842099
Alex Sigman <alex@aiva.ai>
2025-08-27 17:00:06 +0200

    setup file update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("requirements.txt") as f:
 setup(
     name="flipkart_recommender",
     version="0.1",
-    author="Sudhanshu",
+    author="Alex Sigman",
     packages=find_packages(),
     install_requires = requirements,
 )
